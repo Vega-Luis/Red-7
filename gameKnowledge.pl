dd73db412294c6223fbb2bc3f6ff698a4c727e07
Marcos Méndez <marcosmh0199@gmail.com>
2020-11-10 13:30:13 -0600

    fix-gameKnowledge
    all the code were not fixed

--- gameKnowledge.pl
+++ gameKnowledge.pl
@@ -18,31 +18,37 @@ color(violet, X):-
 
 getColor(Card, Color):-
         (
-        Card >= 42,
+        Card >= 42 ->
         color(red, Color)
         ;
-        42 < Card <= 35,
+        42 < Card <= 35 ->
         color(orange, Color)
         ;
-        35 < Card <= 28,
+        35 < Card <= 28 ->
         color(yellow, Color)
         ;
-        28 < Card <= 21,
+        28 < Card <= 21 ->
         color(green, Color)
         ;
-        21 < Card <= 14,
+        21 < Card <= 14 ->
         color(lBlue, Color)
         ;
-        14 < Card <= 7,
+        14 < Card <= 7 ->
         color(purple, Color)
         ;      
-        Card <= 7,
+        Card <= 7 ->
         color(violet, Color)
         ;  
         ).   
 
 getCardNumber(Card, Number):- 
-        Number is Card - (7 * floor(Card /  7)).
+        Result is Card - (7 * floor(Card /  7)),
+        (
+        Result = 0 ->
+        Number is 7
+        ;
+        Number is Result
+        ).
 
 %to make the deck we must create a vector and shuffle it and get the first 14 elements
 createDeck(Deck).
@@ -55,8 +61,9 @@ rule(pairRule, [], Score).
 rule(pairRule, [Head|Tail], Score):- 
 	isPair(Head, Result),
        	Sum is 1 - Result,
-        Total  is Score + sum,
-	rule(pairRule, Tail, Total).	
-
+        Total  is Score + Sum,
+        rule(pairRule, Tail, Total).	
+        
 isPair(Number, Result):- 
-        Result is Number % 2.
+        Result is Number - (2 * floor(Number /  2)).
+
