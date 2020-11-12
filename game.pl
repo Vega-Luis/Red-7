8d38402ffcfc6c0031e4ab4a4a1b19395c6f7caa
Marcos Méndez <marcosmh0199@gmail.com>
2020-11-12 14:27:58 -0600

    feat-players deck

--- game.pl
+++ game.pl
@@ -1,6 +1,27 @@
 :- ensure_loaded("gameKnowledge.pl").
 
+%generate the deck for the game, there is a total of 49 cards
 generateDeck(Deck):-
     numlist(1,49,List),
-    random_permutation(List, Deck).
+    random_permutation(List, Deck). %shuffle list
+
+getSublist([Head|Tail], Result):-
+    length(Tail, X),
+    (
+        X = 7 ->
+        append(Tail,[], Result)
+        ;
+        getSublist(Tail, Result)
+    ).
+
+%generate the deck for the players, each deck size is of 7 cards
+generateDeck(Player, IA):-
+    generateDeck(Cards),
+    reverse(Cards, ReversedCards),
+    writeln(Cards),writeln(ReversedCards),
+    getSublist(Cards, Player),
+    getSublist(ReversedCards, IA).
+    
+    
+
 
