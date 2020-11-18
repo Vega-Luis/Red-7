362d08e17f6e34c41ec5ed6b612ae6b3dfce0e48
Marcos Méndez <marcosmh0199@gmail.com>
2020-11-18 09:56:12 -0600

    feat-getCard

--- game.pl
+++ game.pl
@@ -5,13 +5,22 @@ generateDeck(Deck):-
     numlist(1,49,List),
     random_permutation(List, Deck). %shuffle list
 
-getSublist([Head|Tail], Result):-
+getSublist([_|Tail], Result):-
     length(Tail, X),
     (
-        X = 7 ->
+        X = 8 ->
         append(Tail,[], Result)
         ;
         getSublist(Tail, Result)
+    ).
+
+getCard([Head|Tail], Current, Index, Result):-
+    (
+    Current = Index ->
+    Result is Head
+    ;
+    Current2 is Current + 1,
+    getCard(Tail, Current2, Index, Result)    
     ).
 
 %generate the deck for the players, each deck size is of 7 cards
@@ -21,6 +30,17 @@ generateDeck(Player, IA):-
     writeln(Cards),writeln(ReversedCards),
     getSublist(Cards, Player),
     getSublist(ReversedCards, IA).
+
+game(playerTurn):-
+    writeln("player").
+
+game(iaTurn):-
+    writeln("ia").
+
+game():-
+    generateDeck(Deck),
+    generateDeck(Player, IA).
+
     
     
 
