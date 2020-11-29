292dcf0e9d3f968afb0720c3f8dbb1801b822529
Luis <vrl4@outlook.es>
2020-11-28 19:46:05 -0600

    feat: Adjusment ia

--- iaBehavior.pl
+++ iaBehavior.pl
@@ -4,21 +4,26 @@
 %ver si hay un movimiento que sea mayor al score maximo
 %Necesito retornarle un indice
 %sucks
-nextMove(ActualRule, _, PlayerCards, GameCards, MaxScore, CardIndex, NewRule):-
+nextMove(ActualRule, PlayersPlayedCards, PlayerCards, GameCards, MaxScore, CardIndex, NewRule):-
     cardsToColors(PlayerCards, [], ColorsList),
-    maplist(getCardNumber, PlayerCards, MappedList),
     cardsToRules(ColorsList, [], RulesList),
     getScores(ActualRule, PlayerCards, GameCards, [], ScoreList),
     max_list(ScoreList, NewMax),
     getRuleScores(RulesList, GameCards, [], RuleScoreList),
     max_list(RuleScoreList, MaxRuleScore),
+    nth0(MaxRuleScore, RuleScoreList, NewRuleIndex), 
+    nth0(NewRuleIndex, RulesList, NewRuleChange),
+    %ver si el cambio de regla es conveniente
+    %@PlayerMaxScore lista que contiene la lista de maximos scores por jugador
+    getPlayersPlayedCardsMaxScore([], NewRuleChange, PlayerMaxScore, PlayerMaxScore),
+    max_list(PlayerMaxScore, NewRulePlayersScore),
     (
         (NewMax >= MaxRuleScore, NewMax >= MaxScore) ->
         nth0(CardIndex, ScoreList, NewMax),
         NewRule = ActualRule
     ; 
         (
-        MaxRuleScore > MaxScore ->
+        MaxRuleScore > NewRulePlayersScore ->
         nth0(CardIndex, RuleScoreList, MaxRuleScore),
         nth0(MaxRuleScore, RuleScoreList, NewRuleIndex), 
         nth0(NewRuleIndex, RulesList, NewRule)
@@ -48,3 +53,9 @@ getScores(ActualRule, [PHead|PTail], Move, AuxScoreList, ScoreList):-
 getMoveScore(ActualRule, Move, Score):-
     rule(ActualRule, Move, Score).
 
+getPlayersPlayedCardsMaxScore([], ActtualRule, PlayerMaxScore, PlayerMaxScore).
+
+getPlayersPlayedCardsMaxScore([Head|Tail], ActualRule, TempMaxScore, PlayerScore):-
+    getMoveScore(ActualRule, Head, Score),
+     append(TempMaxScore, [Score], NewList),
+    getPlayersPlayedCardsMaxScore(Tail, ActualRule, NewList, PlayerScore).
